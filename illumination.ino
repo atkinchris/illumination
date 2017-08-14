6d3cead6196ae45c2655b3f15d176ae6255890af
Chris Atkin <atkinchris@gmail.com>
2017-08-14 21:28:24 +0100

    Fade in lights and cycle hues

--- illumination.ino
+++ illumination.ino
@@ -1,29 +1,44 @@
-#include <Adafruit_NeoPixel.h>
+#include <FastLED.h>
 
 #define TOWER_PIN 6
 #define SPOTS_PIN 5
+#define TOWER_LEDS 3
+#define SPOTS_LEDS 1
+#define MAX_VAL 128
+#define START_HUE 171
 
-Adafruit_NeoPixel tower = Adafruit_NeoPixel(3, TOWER_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel spots = Adafruit_NeoPixel(1, SPOTS_PIN, NEO_GRB + NEO_KHZ800);
+CRGB leds[TOWER_LEDS + SPOTS_LEDS];
+CHSV currentColour = CHSV(START_HUE, 196, 0);
 
-uint32_t RED = tower.Color(255, 0, 0);
-uint32_t GRN = tower.Color(0, 255, 0);
-uint32_t BLU = tower.Color(0, 0, 255);
-
-uint32_t colour = tower.Color(255, 0, 128);
-
-void setup() {
-  tower.begin();
-  tower.setBrightness(64);
-  tower.setPixelColor(0, colour);
-  tower.setPixelColor(1, colour);
-  tower.setPixelColor(2, colour);
-  tower.show();
-
-  spots.begin();
-  spots.setBrightness(64);
-  spots.setPixelColor(0, colour);
-  spots.show();
+void setTower(CHSV colour) {
+  fill_solid(leds, TOWER_LEDS, colour);
 }
 
-void loop() {}
+void setSpots(CHSV colour) {
+  fill_solid(leds + TOWER_LEDS, SPOTS_LEDS, currentColour);
+}
+
+void setup() {
+  FastLED.addLeds<NEOPIXEL, TOWER_PIN>(leds, 0, TOWER_LEDS);
+  FastLED.addLeds<NEOPIXEL, SPOTS_PIN>(leds, TOWER_LEDS, SPOTS_LEDS);
+
+  setTower(currentColour);
+  setSpots(currentColour);
+}
+
+void loop() {
+  unsigned long ms = 200;
+
+  if (currentColour.val < MAX_VAL) {
+    currentColour.val += 2;
+    ms = 10;
+  } else {
+    currentColour.hue += 2;
+  }
+
+  setTower(currentColour);
+  setSpots(currentColour);
+
+  FastLED.show();
+  FastLED.delay(ms);
+}
