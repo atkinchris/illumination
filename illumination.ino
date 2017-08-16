528f44bf7a75655cf2bcb41d8a8f02b6b97423f9
Chris Atkin <atkinchris@gmail.com>
2017-08-16 22:34:48 +0100

    Add Clock LED

--- illumination.ino
+++ illumination.ino
@@ -1,29 +1,42 @@
 #include <FastLED.h>
 
-#define TOWER_PIN 6
-#define SPOTS_PIN 5
+#define TOWER_PIN 5
+#define SPOTS_PIN 6
+#define CLOCK_PIN 7
+
 #define TOWER_LEDS 3
 #define SPOTS_LEDS 1
+#define CLOCK_LEDS 1
 #define MAX_VAL 128
 #define START_HUE 171
 
-CRGB leds[TOWER_LEDS + SPOTS_LEDS];
+CRGB spotsLeds[SPOTS_LEDS];
+CRGB towerLeds[TOWER_LEDS];
+CRGB clockLeds[CLOCK_LEDS];
+
 CHSV currentColour = CHSV(START_HUE, 196, 0);
+CHSV clockColour = CHSV(50, 196, 96);
 
 void setTower(CHSV colour) {
-  fill_solid(leds, TOWER_LEDS, colour);
+  fill_solid(towerLeds, TOWER_LEDS, colour);
 }
 
 void setSpots(CHSV colour) {
-  fill_solid(leds + TOWER_LEDS, SPOTS_LEDS, currentColour);
+  fill_solid(spotsLeds, SPOTS_LEDS, colour);
+}
+
+void setClock(CHSV colour) {
+  fill_solid(clockLeds, CLOCK_LEDS, colour);
 }
 
 void setup() {
-  FastLED.addLeds<NEOPIXEL, TOWER_PIN>(leds, 0, TOWER_LEDS);
-  FastLED.addLeds<NEOPIXEL, SPOTS_PIN>(leds, TOWER_LEDS, SPOTS_LEDS);
+  FastLED.addLeds<NEOPIXEL, TOWER_PIN>(towerLeds, TOWER_LEDS);
+  FastLED.addLeds<NEOPIXEL, SPOTS_PIN>(spotsLeds, SPOTS_LEDS);
+  FastLED.addLeds<NEOPIXEL, CLOCK_PIN>(clockLeds, CLOCK_LEDS);
 
   setTower(currentColour);
   setSpots(currentColour);
+  setClock(clockColour);
 }
 
 void loop() {
